ce698214ec1c2f82dbc5d01d46aadba1b871f3ba
Виктор Безунов <victorbezunov@gmail.com>
2022-08-07 23:00:16 +0300

    write AdvertisementController

--- BulletinBoard.API/Controllers/AdvertisementController.cs
+++ BulletinBoard.API/Controllers/AdvertisementController.cs
@@ -1,9 +1,77 @@
-﻿using BulletinBoard.API.Services;
+﻿using BulletinBoard.API.ExtensionClasses;
+using BulletinBoard.API.Models.Advertisement;
+using BulletinBoard.API.Services;
 
 namespace BulletinBoard.API.Controllers
 {
     public class AdvertisementController: ControllerBase<AdvertisementService>
     {
         public AdvertisementController(AdvertisementService service) : base(service) {}
+
+        public async Task GetAdvertisementList(HttpContext ctx)
+        {
+            try
+            {
+                var request = await ctx.Request.ReadFromJsonAsync<GetAdvertisementListRequest>();
+                var response = _service.GetAdvertisementList(request);
+                await ctx.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception e)
+            {
+                await SenderError.Error500(ctx, e);
+            }
+        }
+        public async Task GetAdvertisementDetail(HttpContext ctx)
+        {
+            try
+            {
+                var request = await ctx.Request.ReadFromJsonAsync<GetAdvertisementDetailRequest>();
+                var response = _service.GetAdvertisementDetail(request);
+                await ctx.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception e)
+            {
+                await SenderError.Error500(ctx, e);
+            }
+        }
+        public async Task CreateAdvertisement(HttpContext ctx)
+        {
+            try
+            {
+                var request = await ctx.Request.ReadFromJsonAsync<CreateAdvertisementRequest>();
+                var response = _service.CreateAdvertisement(request);
+                await ctx.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception e)
+            {
+                await SenderError.Error500(ctx, e);
+            }
+        }
+        public async Task UpdateAdvertisement(HttpContext ctx)
+        {
+            try
+            {
+                var request = await ctx.Request.ReadFromJsonAsync<UpdateAdvertisementRequest>();
+                var response = _service.UpdateAdvertisement(request);
+                await ctx.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception e)
+            {
+                await SenderError.Error500(ctx, e);
+            }
+        }
+        public async Task RemoveAdvertisement(HttpContext ctx)
+        {
+            try
+            {
+                var request = await ctx.Request.ReadFromJsonAsync<RemoveAdvertisementRequest>();
+                var response = _service.RemoveAdvertisement(request);
+                await ctx.Response.WriteAsJsonAsync(response);
+            }
+            catch (Exception e)
+            {
+                await SenderError.Error500(ctx, e);
+            }
+        }
     }
 }
